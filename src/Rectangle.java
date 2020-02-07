a28485779c3e2e086f09fe8fc8cac6af56defb17
Martijn Leus <m.h.m.leus@student.tue.nl>
2020-02-07 11:00:45 +0100

    Initial version of Rectangle class.

---
+++ src/Rectangle.java
@@ -0,0 +1,33 @@
+class Rectangle {
+    public int w, h;
+    public int x, y;
+    public boolean rotated;
+
+    /**
+     * A rectangle widht a specified width and height.
+     */
+    public Rectangle(int w, int h) {
+        this.w = w;
+        this.h = h;
+
+        /* Set position to (-1, -1) to catch potential errors. */
+        this.x = this.y = -1;
+        this.rotated = false;
+    }
+
+    public int getX() { return this.x; }
+    public void setX(int x) { this.x = x; }
+
+    public int getY() { return this.y; }
+    public void setY(int y) { this.y = y; }
+
+    /**
+     * Rotate the rectangle by swapping width and height.
+     */
+    public void rotate() {
+        this.rotated = !this.rotated;
+        int tmp = w;
+        this.w = this.h;
+        this.h = tmp;
+    }
+}
