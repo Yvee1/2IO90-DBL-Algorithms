e88c364bbfc7b65ff465fcbc4851552f677661de
Martijn Leus <m.h.m.leus@student.tue.nl>
2020-02-07 11:44:17 +0100

    Allow setting size in PackingSolution constructor.

--- src/PackingSolution.java
+++ src/PackingSolution.java
@@ -5,4 +5,10 @@ public class PackingSolution {
 
     public PackingSolution(Rectangle[] solution) { this.solution = solution; }
 
+    public PackingSolution(int width, int height, Rectangle[] solution) {
+        this(solution);
+        this.w = width;
+        this.h = height;
+    }
+
 }
