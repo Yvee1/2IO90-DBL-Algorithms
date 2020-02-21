8e629789ad50ea0dbe9c5562a9006e014e312e4b
Vincent <vincentmoonen@hotmail.com>
2020-02-21 12:25:00 +0100

    Documenting Steinberg

--- PackingSolver/src/PackingSolver.java
+++ PackingSolver/src/PackingSolver.java
@@ -1,10 +1,13 @@
-
 import java.util.*;
-
 //import Visualizer;
+
 public class PackingSolver {
+
     static Scanner sc;
 
+    /**
+     * The primary function called by main
+     */
     static void run() {
 
         // Read the problem from inpit
@@ -35,15 +38,26 @@ public class PackingSolver {
         //Visualizer.visualize(sol);
     }
 
+    /**
+     * Checks the validity of a solution by checking for overlaps and checking if every
+     * rectangle adheres to the height limit
+     *
+     * @param rects the rectangles to run the checks on
+     * @param settings the settings applicable to these rectangles
+     */
     public static void checkValidity(Rectangle[] rects, PackingSettings settings) {
+        // For every rectangle
         for (int i = 0; i < rects.length; i++) {
             Rectangle r1 = rects[i];
+            // Check if height limit is satisfied
             if (r1.getHeight() + r1.getY() > settings.maxHeight) {
                 System.out.println("MAX HEIGHT VIOLATED: " + i);
             }
+            // For every pair of rectangles
             for (int j = 0; j < rects.length; j++) {
                 Rectangle r2 = rects[j];
                 if (r1 == r2) { continue; }
+                // Check if they overlap
                 if (r1.getX() >= r2.getX() + r2.getWidth() || r2.getX() >= r1.getX() + r1.getWidth()) {
                     continue;
                 }
